9b17701e0ed958f56d7d560ac2ac7dd93e172925
Dennis Somerville <dennis.somerville@gmail.com>
2013-07-24 11:07:41 -0400

    Cart item introduced.

---
+++ Katas.SupermarketKata.Tests/CartTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Katas.SupermarketKata.Tests
+{
+    [TestFixture]
+    public class CartTests
+    {
+        private const decimal LoafOfBreadPrice = 1m;
+
+        [Test]
+        public void GetTotal_EmptyCartReturnsZero()
+        {
+            var cart = new Cart();
+            Assert.That(cart.Total(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GetTotal_LoafOfBread()
+        {
+            var cart = new Cart();
+            cart.Add(new LoafOfBread());
+            Assert.That(cart.Total(), Is.EqualTo(LoafOfBreadPrice));
+        }
+        
+    }
+
+    [TestFixture]
+    public class CartItemTests
+    {
+        [Test]
+        public void CreateCartItem()
+        {
+            var loafOfBread = new LoafOfBread();
+            var item = new CartItem(loafOfBread, 1);
+            Assert.That(item.Quantity, Is.EqualTo(1));
+            Assert.AreSame(item.Product, loafOfBread);
+        }
+    }
+
+    [TestFixture]
+    public class ProductTests
+    {
+        private const decimal PriceLoadOfBread = 1m;
+
+        [Test]
+        public void CreateProduct()
+        {
+            var bread = new LoafOfBread();
+            Assert.That(bread.Price, Is.EqualTo(PriceLoadOfBread));
+        }
+    }
+
+    
+}
---
+++ Katas.SupermarketKata/Cart.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas.SupermarketKata
+{
+    public class Cart
+    {
+        List<CartItem> _cartItems = new List<CartItem>(); 
+
+        public decimal Total()
+        {
+            return _cartItems.Sum(item => item.Product.Price * item.Quantity);
+        }
+
+        public void Add(IProduct product)
+        {
+            var item = new CartItem(product, 1);
+            _cartItems.Add(item);
+        }
+    }
+
+    public class CartItem
+    {
+        public CartItem(IProduct product, int quantity)
+        {
+            Quantity = quantity;
+            Product = product;
+        }
+
+        public int Quantity { get; set; }
+        public IProduct Product { get; private set; }
+    }
+
+    public class LoafOfBread : IProduct
+    {
+        private const decimal LoafOfBreadPrice = 1m;
+
+        public decimal Price
+        {
+            get { return LoafOfBreadPrice; }
+        }
+    }
+
+    public interface IProduct
+    {
+        decimal Price { get; }
+    }
+}
